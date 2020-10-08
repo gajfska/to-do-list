06a6a86e026697e32a0f8119687d4ebecbfe9deb
Werka <weronika.gajska@gmail.com>
2020-10-08 15:18:24 +0200

    installing Material Design and creating simple table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+export interface TaskElement {
+    taskName: string;
+    priority: string;
+    done: string;
+}
+
+
+const ELEMENT_DATA: TaskElement[] = [
+    { taskName: 'Feed turtle', priority: 'Low', done: 'x'},
+    { taskName: 'Vacuum', priority: 'Medium', done: 'x'},
+    { taskName: 'Wash the dishes', priority: 'Low', done: 'x'},
+    { taskName: 'Make dinner', priority: 'Low', done: 'x'},
+    { taskName: 'Exercise', priority: 'Medium', done: 'x'},
+    { taskName: 'Do the laundry', priority: 'Low', done: 'x'},
+    { taskName: 'Water the flowers', priority: 'High', done: 'x'},
+    { taskName: 'Do shopping', priority: 'High', done: 'x'},
+    { taskName: 'Play board games', priority: 'Medium', done: 'x'},
+    { taskName: 'Take a nap', priority: 'Medium', done: 'x'},
+];
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'to-do-list';
+
+
+  displayedColumns: string[] = ['taskName', 'priority', 'done'];
+  dataSource = ELEMENT_DATA;
+
 }
